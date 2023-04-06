b959b7614f795cdec46fd485ef2316bd80a23731
chinbor Mn <chinbor.Mn@gmail.com>
2023-04-06 17:12:51 +0800

    fix: don't copy readme

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,8 @@
 import { execSync } from 'node:child_process'
-import path from 'node:path'
 import consola from 'consola'
-import { copyFileSync } from 'fs-extra'
-import { automneVuePkgRoot, repoRoot } from './paths'
+import { repoRoot } from './paths'
 
 execSync('pnpm run clean', { stdio: 'inherit', cwd: repoRoot })
 execSync('pnpm --filter automne-vue build', { stdio: 'inherit', cwd: repoRoot })
-
-// copy README.md file to dist dir for npm publish
-copyFileSync(
-  path.join(repoRoot, 'README.md'),
-  path.join(automneVuePkgRoot, './dist/README.md'),
-)
 
 consola.success('Build complete!')
